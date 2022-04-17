7571d08d6f33e966d706cd0a981bca9f346b7cbd
柚子Youzi <Yoyowu9601@gmail.com>
2022-04-17 10:24:45 +0800

    fun 系列隱藏

--- Root/Commands/SlashCommands/Fun/fun.js
+++ Root/Commands/SlashCommands/Fun/fun.js
@@ -10,6 +10,7 @@ const {
 module.exports = {
     name: 'fun',
     description: '遊戲系列',
+    options: [],
     ignoreFile: true,
     run: async (client, interaction, container) => {
         // 內容
