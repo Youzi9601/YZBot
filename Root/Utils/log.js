c67a72866f627756cc00c068fd618456a483fd92
柚子Youzi <Yoyowu9601@gmail.com>
2022-05-04 23:29:54 +0800

    調整漏洞

--- Root/Utils/log.js
+++ Root/Utils/log.js
@@ -11,30 +11,33 @@ const { Client } = require('discord.js');
  * @param {Client} client 機器人
  */
 module.exports = (level, msg, client) => {
-    const log_channel = client.channels.cache.get(
-        config.Channels.All,
-    );
-    log_channel.send(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${config.console_prefix} [${level}] ${msg}`,
-    );
+    if (config.Channels.All) {
+        const log_channel = client.channels.cache.get(
+            config.Channels.All,
+        );
+        // 發送訊息
+        log_channel.send(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${config.console_prefix} [${level}] ${msg}`,
+        );
+    }
     if (level == 'info') {
         console.log(chalk.gray(
             `[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${config.console_prefix}`,
         ) + msg);
-        fs.appendFile(`logs/${moment().format('YYYY-MM-DD')}.log`, `\n[${moment().format('YYYY-MM-DD HH:mm:ss')}] INFO｜${msg}`, function(err) {
+        fs.appendFile(`logs/${moment().format('YYYY-MM-DD')}.log`, `\n[${moment().format('YYYY-MM-DD HH:mm:ss')}] INFO｜${msg}`, function (err) {
             // none
         });
     } else if (level == 'warn') {
         console.warn(chalk.gray(
             `[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${config.console_prefix}`,
         ) + msg);
-        fs.appendFile(`logs/${moment().format('YYYY-MM-DD')}.log`, `\n[${moment().format('YYYY-MM-DD HH:mm:ss')}] WARN｜${msg}`, function(err) {
+        fs.appendFile(`logs/${moment().format('YYYY-MM-DD')}.log`, `\n[${moment().format('YYYY-MM-DD HH:mm:ss')}] WARN｜${msg}`, function (err) {
             // none
         });
     } else if (level == 'error') {
         console.error(chalk.gray(
             `[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${config.console_prefix}`,
         ) + msg);
-        fs.appendFile(`logs/${moment().format('YYYY-MM-DD')}.log`, `\n[${moment().format('YYYY-MM-DD HH:mm:ss')}] ERROR｜${msg}`, function(err) {
+        fs.appendFile(`logs/${moment().format('YYYY-MM-DD')}.log`, `\n[${moment().format('YYYY-MM-DD HH:mm:ss')}] ERROR｜${msg}`, function (err) {
             // none
         });
     }
