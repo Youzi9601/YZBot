ef8d7c1b6b1d98363f083de5b5ba0feef8df652a
ESLint <lint-action@samuelmeuli.com>
2022-05-08 06:54:01 +0000

    使用 ESLint 改進代碼質量

--- Root/Events/Guild/guildDelete.js
+++ Root/Events/Guild/guildDelete.js
@@ -10,8 +10,8 @@ module.exports = {
     once: false,
     run: async (guild, client, container) => {
         const discordmsg = {
-            content: `進退變動 > 加入 ${guild.name}`
-        }
+            content: `進退變動 > 加入 ${guild.name}`,
+        };
         log(
             'info',
             chalk.green('進退變動 > ') + `離開 ${guild.name}`,
