8805c95e311ce0aaccca09a3308d1a02803b6798
Youzi9601 <yoyowu9601@gmail.com>
2022-05-11 18:29:09 +0800

    測試版

--- pm2.js
+++ pm2.js
@@ -1,5 +1,17 @@
 (async () => {
-    // 下載npm
+    const sleep = async (ms) => {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve();
+            }, ms || 0);
+        });
+    };
+    console.log('啟動')
     const exec = require('child_process').exec;
     await exec('pm2 start bot.js --watch --name "YZB"');
-});
+    for (let index = 0; true; index++) {
+        const element = array[index];
+        await sleep(30000);
+    } // 等待
+
+})
