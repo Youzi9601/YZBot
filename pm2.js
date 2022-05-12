fb2737f59c70e76ea890d8153a0a7a6c0ee09e59
Youzi9601 <yoyowu9601@gmail.com>
2022-05-12 08:14:16 +0800

    調整

--- pm2.js
+++ pm2.js
@@ -8,7 +8,7 @@
     };
     console.log('啟動');
     const exec = require('child_process').exec;
-    await exec('pm2 start bot.js --watch --name "YZB"');
+    await exec('npm run prod:start');
     for (let index = 0; true; index++) {
         const element = array[index];
         await sleep(30000);
