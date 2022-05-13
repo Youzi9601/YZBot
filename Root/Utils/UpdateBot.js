2529fd7c61f13b545fbdd1703a2d25511d6b3334
Youzi9601 <yoyowu9601@gmail.com>
2022-05-13 12:18:02 +0800

    調整updater

--- Root/Utils/UpdateBot.js
+++ Root/Utils/UpdateBot.js
@@ -11,29 +11,29 @@ async function update() {
     if (fs.existsSync('./.git')) {
         const exec = require('child_process').exec;
         await exec('git reset --hard');
-        console.log('\x1b[34m%s\x1b[0m', '更新......');
+        console.log('\x1b[34m%s\x1b[0m', '[基本作業]開始檢查更新......');
         await exec('git pull', (err, stdout, stderr) => {
             if (err) {
-                console.log('\x1b[31m%s\x1b[0m', '錯誤: ' + err);
+                console.log('\x1b[31m%s\x1b[0m', '[基本作業]錯誤: ' + err);
                 return;
             }
             // console.log('' + stdout + '');
             if (!stdout.includes('Already up to date.')) {
-                console.log('\x1b[32m%s\x1b[0m', '更新成功！請重新啟動！');
+                console.log('\x1b[32m%s\x1b[0m', '[基本作業]更新成功！請重新啟動！');
                 process.exit(0);
             } else {
-                console.log('\x1b[32m%s\x1b[0m', '沒有新的更新！');
+                console.log('\x1b[32m%s\x1b[0m', '[基本作業]沒有新的更新！');
             }
 
         });
-        console.log('\x1b[34m%s\x1b[0m', '安裝依賴項......');
+        console.log('\x1b[34m%s\x1b[0m', '[基本作業]安裝依賴項......');
         childProcess.exec('npm install', (err, stdout, stderr) => {
             if (err) {
-                console.log('\x1b[31m%s\x1b[0m', '錯誤: ' + err);
+                console.log('\x1b[31m%s\x1b[0m', '[基本作業]錯誤: ' + err);
                 return;
             }
             console.log('' + stdout + '');
-            console.log('\x1b[32m%s\x1b[0m', 'Package.json 中的依賴項安裝完成！');
+            console.log('\x1b[32m%s\x1b[0m', '[基本作業] Package.json 中的依賴項安裝完成！');
             // exec('node bot.js');
 
         });
