bf5e0cb17555847cdf94dbea0136139443152342
ESLint <lint-action@samuelmeuli.com>
2022-05-14 13:12:53 +0000

    使用 ESLint 格式化代碼

--- Root/Structures/CommandOptions/loadCommandOptions.js
+++ Root/Structures/CommandOptions/loadCommandOptions.js
@@ -14,7 +14,7 @@ const { log } = require('../../Utils/log');
  * @param {import('discord.js').IntegrationType} interactionType
  * @returns
  */
-module.exports = async function (
+module.exports = async function(
     client,
     message,
     command,
@@ -39,8 +39,8 @@ module.exports = async function (
         true,
         client,
         undefined,
-        config.Channels.commandRec
-    )
+        config.Channels.commandRec,
+    );
 
     // 檢查是否有以下指令設定
     if (await require('./OnlyRunOnGuilds')(message, command, Discord)) return;
