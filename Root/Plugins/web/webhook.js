5c24efbe21459c1026326355447204c0fee6fbc7
Youzi9601 <yoyowu9601@gmail.com>
2022-05-18 19:35:02 +0800

    調整

--- Root/Plugins/web/webhook.js
+++ Root/Plugins/web/webhook.js
@@ -10,16 +10,17 @@ module.exports = (client) => {
 
     const webhook = new Topgg.Webhook(Config.webhook.authorization); // 添加您的 Top.gg webhook 授權（不是機器人令牌）
 
-    app.post('/webhook', webhook.listener(vote => {
-        // vote 是你的投票對象
-        console.log(vote); // 使用者id
-        // 以下為執行工作
-
-        // --
-    }),
-    function(req, res) {
+    app.post('/webhook', function (req, res) {
         res.status(200).send('成功！');
     },
+
+        webhook.listener(vote => {
+            // vote 是你的投票對象
+            console.log(vote); // 使用者id
+            // 以下為執行工作
+
+            // --
+        }),
     ); // 附加中間件
 
     const port = Number(Config.webhook.port);
