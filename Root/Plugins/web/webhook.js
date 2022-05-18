7c5d25efb3f195e9a91a9969e16903de05380367
Youzi9601 <yoyowu9601@gmail.com>
2022-05-18 19:29:08 +0800

    調整問題

--- Root/Plugins/web/webhook.js
+++ Root/Plugins/web/webhook.js
@@ -1,33 +1,30 @@
 module.exports = (client) => {
 
-    const Topgg = require('@top-gg/sdk');
-    const Config = require('../../../Config');
+    const Topgg = require('@top-gg/sdk')
+    const Config = require('../../../Config')
 
     var http = require('http'),
         https = require('https'),
         express = require('express'),
         app = express();
 
-    const webhook = new Topgg.Webhook(Config.webhook.authorization);
-    // 添加您的 Top.gg webhook 授權（不是機器人令牌）
+    const webhook = new Topgg.Webhook(Config.webhook.authorization) // 添加您的 Top.gg webhook 授權（不是機器人令牌）
 
-    app.post('/webhook', async (request) => {
-        // 拿到 Webhook 的 JSON 格式資料
-        console.log(request);
-        return response({ status: 200 });
-    });
-    // 附加中間件
-    const port = Number(Config.webhook.port);
+    app.post('/webhook', webhook.listener(vote => {
+        // vote 是你的投票對象
+        console.log(vote) // 使用者id
+        // 以下為執行工作
+
+        // --
+    }),
+        function (req, res) {
+            res.status(200).send('成功！')
+        }
+    ) //附加中間件
+
+    const port = Number(Config.webhook.port)
     // app.listen(port) //你的港口
     http.createServer(app).listen(port);
-    console.log(` -> Webhook 接收於 Port:${port}`);
+    console.log(` -> Webhook 接收於 Port:${port}`)
 
-};
-
-function response({ data = '', status = 200, headers = {} }) {
-    const hander = new Headers();
-    return new Response(data, {
-        status,
-        headers: header,
-    });
 }
