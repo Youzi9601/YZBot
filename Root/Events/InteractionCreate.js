d5e42a0019a600dbe350f0b400a60567ae83ead2
Youzi9601 <yoyowu9601@gmail.com>
2022-05-20 14:15:05 +0800

    修正trycatch問題

--- Root/Events/InteractionCreate.js
+++ Root/Events/InteractionCreate.js
@@ -2,6 +2,7 @@ const moment = require('moment');
 const chalk = require('chalk');
 const fs = require('fs');
 const { CommandInteraction, Client } = require('discord.js');
+const { log } = require('../Utils/log');
 module.exports = {
     name: 'interactionCreate',
     /**
@@ -10,10 +11,15 @@ module.exports = {
      * @param {import('discord.js').Client} client 機器人
      */
     run: async (interaction, client) => {
-        const loadCommandOptions = require('../Structures/CommandOptions/loadCommandOptions');
-        if (interaction.isButton()) loadCommandOptions(client, interaction, client.commands.buttonCommands.get(interaction.customId), true, 'Button');
-        else if (interaction.isSelectMenu()) loadCommandOptions(client, interaction, client.commands.selectMenus.get(interaction.values[0] ?? interaction.customId), true, 'SelectMenus');
-        else if (interaction.isCommand()) loadCommandOptions(client, interaction, client.commands.slashCommands.get(interaction.commandName), true, 'SlashCommand');
-        else if (interaction.isContextMenu()) loadCommandOptions(client, interaction, client.commands.contextMenus.get(interaction.commandName), true, 'ContextMenus');
+        try {
+            const loadCommandOptions = require('../Structures/CommandOptions/loadCommandOptions');
+            if (interaction.isButton()) loadCommandOptions(client, interaction, client.commands.buttonCommands.get(interaction.customId), true, 'Button');
+            else if (interaction.isSelectMenu()) loadCommandOptions(client, interaction, client.commands.selectMenus.get(interaction.values[0] ?? interaction.customId), true, 'SelectMenus');
+            else if (interaction.isCommand()) loadCommandOptions(client, interaction, client.commands.slashCommands.get(interaction.commandName), true, 'SlashCommand');
+            else if (interaction.isContextMenu()) loadCommandOptions(client, interaction, client.commands.contextMenus.get(interaction.commandName), true, 'ContextMenus');
+        } catch (error) {
+            interaction.reply(':x: 啊喔...發生了錯誤 :/\n\`\`\`' + error + '\n\`\`\`')
+            log('error', '發生了錯誤！\n' + error, ture, client)
+        }
     },
 };
