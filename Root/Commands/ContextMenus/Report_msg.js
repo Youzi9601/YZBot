2fa8aac280e1f9d79ac0ba57cc903078e56f2acb
Youzi9601 <yoyowu9601@gmail.com>
2022-05-23 18:52:50 +0800

    調整訊息

--- Root/Commands/ContextMenus/Report_msg.js
+++ Root/Commands/ContextMenus/Report_msg.js
@@ -1,3 +1,4 @@
+
 module.exports = {
     name: '舉報訊息',
     type: 'MESSAGE',
@@ -6,11 +7,14 @@ module.exports = {
             interaction.channel.messages.cache.get(interaction.targetId) ??
             (await interaction.channel.messages.fetch(interaction.targetId)),
         );
-        const embed = new Discord.MessageEmbed()
-            .setTitle('舉報成功！')
-            .setThumbnail(client.user.displayAvatarURL({ dynamic: true }))
-            .setColor('RANDOM')
-            .setDescription('我們將會盡快處理！感謝您的配合！');
-        interaction.reply({ embeds: [embed], ephemeral: true });
+        /* 
+        const embed = new container.Discord.MessageEmbed()
+             .setTitle('舉報成功！')
+             .setThumbnail(client.user.displayAvatarURL({ dynamic: true }))
+             .setColor('RANDOM')
+             .setDescription('我們將會盡快處理！感謝您的配合！');
+             */
+        // embeds: [embed], ephemeral: true
+        interaction.reply({ content: `這個功能正在開發中...@@` });
     },
 };
