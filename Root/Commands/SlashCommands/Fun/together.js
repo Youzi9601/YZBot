596e466b1f45c973682ea98d6f07a66ef0b816f7
ESLint <lint-action@samuelmeuli.com>
2022-05-24 03:12:56 +0000

    使用 ESLint 格式化代碼

--- Root/Commands/SlashCommands/Fun/together.js
+++ Root/Commands/SlashCommands/Fun/together.js
@@ -120,27 +120,27 @@ module.exports = {
             const embed = new MessageEmbed()
                 .setTitle('啊喔...')
                 .setDescription('你不在語音頻道！請先加入語音頻道後再使用！')
-                .setColor('RED')
-            return interaction.reply({ embeds: [embed] })
+                .setColor('RED');
+            return interaction.reply({ embeds: [embed] });
 
             // 命令
         } else if (interaction.member.voice.channel.type == 'GUILD_STAGE_VOICE') {
             const embed = new MessageEmbed()
                 .setTitle('啊喔...')
                 .setDescription('你不在語音頻道！Together 命令不支援於 **舞台頻道** ！')
-                .setColor('RED')
-            return interaction.reply({ embeds: [embed] })
+                .setColor('RED');
+            return interaction.reply({ embeds: [embed] });
 
         } else client.discordTogether.createTogetherCode(interaction.member.voice.channel.id, `${subcommand}`).then(async invite => {
 
-            const voice_channel = interaction.guild.members.cache.get(client.user.id).voice.channel
+            const voice_channel = interaction.guild.members.cache.get(client.user.id).voice.channel;
 
             if (voice_channel && voice_channel.id != interaction.member.voice.channel.id) {
 
                 interaction.channel.send(`一起在這裡！ \n> ${invite.code}`);
                 interaction.deferReply({ ephemeral: true }).then(cmd => {
-                    interaction.editReply({ content: '因為我目前在其他的頻道中...所以無法加入您的頻道！' })
-                })
+                    interaction.editReply({ content: '因為我目前在其他的頻道中...所以無法加入您的頻道！' });
+                });
             } else {
                 const { joinVoiceChannel } = require('@discordjs/voice');
                 joinVoiceChannel({
@@ -150,8 +150,8 @@ module.exports = {
                 });
                 interaction.channel.send(`一起在這裡！ \n> ${invite.code}`);
                 interaction.deferReply().then(cmd => {
-                    interaction.deleteReply()
-                })
+                    interaction.deleteReply();
+                });
             }
         });
 
