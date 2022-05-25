ea82e99784d04d33dbb713bfbdc176a6a74f19ff
ESLint <lint-action@samuelmeuli.com>
2022-05-25 07:21:11 +0000

    使用 ESLint 格式化代碼

--- Root/Commands/SlashCommands/Owner/developers.js
+++ Root/Commands/SlashCommands/Owner/developers.js
@@ -61,13 +61,13 @@ module.exports = {
 
         // #region list-servers
         if (subcommand == 'list-servers') {
-            const { MessageEmbed } = require('discord.js')
+            const { MessageEmbed } = require('discord.js');
             let i = 1;
             let page = 1;
-            let embed = [];
+            const embed = [];
             let field = [];
             client.guilds.cache.forEach(
-                /**@param {import('discord.js').Guild} guild */
+                /** @param {import('discord.js').Guild} guild */
                 (guild) => {
 
                     if (Math.round(i / 25) == i / 25) {
@@ -75,8 +75,8 @@ module.exports = {
                             new MessageEmbed()
                                 .setTitle(`第${page}頁`)
                                 .addFields(field)
-                                .setColor('WHITE')
-                        )
+                                .setColor('WHITE'),
+                        );
                         field = [];
                         page++;
                     }
@@ -85,11 +85,11 @@ module.exports = {
                             name: `${i}. ${guild.name} (${guild.id})`,
                             value: [
                                 ` 所有者 ${guild.ownerId}`,
-                                `總共${guild.memberCount}人 | 成員${guild.members.cache.filter((m) => !m.user.bot).size}人 | 機器人${guild.members.cache.filter((m) => m.user.bot).size}人`
+                                `總共${guild.memberCount}人 | 成員${guild.members.cache.filter((m) => !m.user.bot).size}人 | 機器人${guild.members.cache.filter((m) => m.user.bot).size}人`,
                             ].join('\n'),
                             inline: true,
-                        }
-                    )
+                        },
+                    );
 
 
                     console.info(
@@ -104,20 +104,20 @@ module.exports = {
 
                     i++;
                 });
-            console.log('embed')
-            console.log(embed)
-            console.log('field')
-            console.log(field)
+            console.log('embed');
+            console.log(embed);
+            console.log('field');
+            console.log(field);
 
             if (field != []) {
                 embed.push(
                     new MessageEmbed()
                         .setTitle(`第${page}頁`)
                         .addFields(field)
-                        .setColor('WHITE')
-                )
+                        .setColor('WHITE'),
+                );
             }
-            console.log(embed)
+            console.log(embed);
 
             // 發送訊息
             const msg = new container.Discord.MessageEmbed()
@@ -129,7 +129,7 @@ module.exports = {
                 })
                 .setDescription('伺服器列表已列於控制台！');
             // 添加到最前端
-            embed.unshift(msg)
+            embed.unshift(msg);
             interaction.reply({
                 embeds: embed,
                 allowedMentions: {
@@ -139,23 +139,23 @@ module.exports = {
             });
             // interaction.channel.send({ embeds: embed })
         } else
-            // #endregion
+        // #endregion
 
-            // #region leave-server
-            if (subcommand == 'leave-server') {
-                const id = interaction.options.getString('id');
-                const guild = client.guilds.cache.get(id);
-                try {
-                    if (!guild) {
-                        return interaction.reply({ content: '未指定伺服器 ID。請指定伺服器ID' });
-                    }
-
-                    await guild.leave();
-                    interaction.reply({ content: `成功離開 **${guild.name}**，少了\`${guild.memberCount}\`位成員。` });
-                } catch (err) {
-                    interaction.reply({ content: `離開伺服器時發生錯誤： \`${err.message}\`` });
+        // #region leave-server
+        if (subcommand == 'leave-server') {
+            const id = interaction.options.getString('id');
+            const guild = client.guilds.cache.get(id);
+            try {
+                if (!guild) {
+                    return interaction.reply({ content: '未指定伺服器 ID。請指定伺服器ID' });
                 }
+
+                await guild.leave();
+                interaction.reply({ content: `成功離開 **${guild.name}**，少了\`${guild.memberCount}\`位成員。` });
+            } catch (err) {
+                interaction.reply({ content: `離開伺服器時發生錯誤： \`${err.message}\`` });
             }
+        }
         // #endregion
         // #region exit
         if (subcommand == 'exit') {
