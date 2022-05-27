1264fb53006babeb284d0f23b7cefeb550ce1055
ESLint <lint-action@samuelmeuli.com>
2022-05-27 01:15:26 +0000

    使用 ESLint 格式化代碼

--- Root/Commands/ContextMenus/Report_msg.js
+++ Root/Commands/ContextMenus/Report_msg.js
@@ -15,30 +15,30 @@ module.exports = {
             .setCustomId('report_message')
             .setTitle('舉報訊息');
         // 將組件添加到模態
-        //創建文本輸入組件
+        // 創建文本輸入組件
         const SuggestionsInput = new TextInputComponent()
             .setCustomId('reason')
             // 設定為必填
             .setRequired(true)
-            //標籤是用戶看到此輸入的提示
-            .setLabel("為何舉報此訊息?")
+            // 標籤是用戶看到此輸入的提示
+            .setLabel('為何舉報此訊息?')
             // 短意味著只有一行文本
             .setStyle('SHORT')
             .setPlaceholder('例：不雅字眼、違反社群規定...')
             .setMinLength(2)
-            .setMaxLength(20)
+            .setMaxLength(20);
         const OthersInput = new TextInputComponent()
             .setCustomId('whatelse')
-            .setLabel("還有其他的嗎?")
-            //段落表示多行文字。
+            .setLabel('還有其他的嗎?')
+            // 段落表示多行文字。
             .setStyle('PARAGRAPH')
             .setPlaceholder('如果還有其他的東西，可在這裡做詳細的說明')
-            .setValue('無')
+            .setValue('無');
         // 一個動作行只包含一個文本輸入，
         // 所以每個文本輸入需要一個操作行。
         const firstActionRow = new MessageActionRow().addComponents(SuggestionsInput);
         const secondActionRow = new MessageActionRow().addComponents(OthersInput);
-        //向模態添加輸入
+        // 向模態添加輸入
         modal.addComponents(firstActionRow, secondActionRow);
         // 向用戶顯示模態
         await interaction.showModal(modal);
