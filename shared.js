1de81979d2cc8b55b3d6189a16a8d787f59eef78
Youzi9601 <yoyowu9601@gmail.com>
2023-02-03 17:10:28 +0800

    修復了下拉選單的錯誤問題以及修正了shared.js的部分日誌輸出

--- shared.js
+++ shared.js
@@ -19,27 +19,42 @@ const manager = new ShardingManager('./bot.js',
     },
 );
 
-manager.on('shardCreate', shard => console.log(`[#${shard.id}]  啟動分片 #${ shard.id }`));
 
+manager.on('shardCreate', shard => {
+    console.log(`[#${ shard.id }]  啟動分片 #${ shard.id }`)
+    shard.on('ready', r => {
+        console.log("分片 #" + shard.id + " 已回傳完成啟動");
+    })
+
+    shard.on('message', message => {
+        console.log(`[#${shard.id}]  ${message}`);
+    });
+
+    shard.on("death", (process) => {
+        console.error("分片 #" + shard.id + " 意外關閉！ PID：" + process.pid + "; 退出代碼：" + process.exitCode + ".");
+
+        if (process.exitCode === null) {
+            console.warn("警告: 分片 #" + shard.id + " 以 null 錯誤代碼退出。這可能是缺少可用系統內存的結果。確保分配了足夠的內存以繼續。");
+        }
+    });
+    shard.on('reconnecting', (event) => {
+        console.warn("分片 #" + shard.id + " 正在重新連接...");
+        console.log(event);
+    });
+    shard.on("disconnect", (event) => {
+        console.warn("分片 #" + shard.id + " 斷開連接。正在轉儲套接字關閉事件...");
+        console.log(event);
+    });
+    shard.on('error', err => {
+        console.error("分片 #" + shard.id + " 發生錯誤：" + err.name);
+        console.error(err.message);
+        if (err.stack) console.error(err.stack)
+    })
+});
 
 manager
     .spawn()
-    .then(shards => {
-        shards.forEach(shard => {
-            shard.on('message', message => {
-                console.log(`[#${shard.id}]  ${message}`);
-            });
-            shard.on("death", (process) => {
-                console.error("分片 #" + shard.id + " 意外關閉！ PID：" + process.pid + "; 退出代碼：" + process.exitCode + ".");
-
-                if (process.exitCode === null) {
-                    console.warn("警告: 分片 #" + shard.id + " 以 NULL 錯誤代碼退出。這可能是缺少可用系統內存的結果。確保分配了足夠的內存以繼續。");
-                }
-            });
-            shard.on("disconnect", (event) => {
-                console.warn("分片 #" + shard.id + " 斷開連接。正在轉儲套接字關閉事件...");
-                console.log(event);
-            });
-        });
+    .then((_shards) => {
+        console.log('成功啟動' + manager.totalShards + '個分片！')
     })
     .catch(console.error);
