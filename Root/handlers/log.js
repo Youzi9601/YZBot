6b4103f4585dbd9ed07e82885a26e9c1d9787c1f
Youzi9601 <yoyowu9601@gmail.com>
2023-02-28 15:25:27 +0800

    架構日誌紀錄系統

---
+++ Root/handlers/log.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+
+/**
+ *
+ * @param {import('discord.js').Client} client
+ * @param {*} config
+ */
+module.exports = (client, config) => {
+// 暫定作為日誌紀錄系統 => client.log()
+};
