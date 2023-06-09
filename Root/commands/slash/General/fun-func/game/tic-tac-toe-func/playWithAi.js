57e43bd76367c43d2519175a5073c7218c52cd72
柚子Youzi <93034834+Youzi9601@users.noreply.github.com>
2023-06-09 11:51:51 +0000

    新增拋出錯誤

--- Root/commands/slash/General/fun-func/game/tic-tac-toe-func/playWithAi.js
+++ Root/commands/slash/General/fun-func/game/tic-tac-toe-func/playWithAi.js
@@ -7,6 +7,7 @@ const { ComponentType, EmbedBuilder, ActionRowBuilder, StringSelectMenuBuilder, 
  */
 module.exports = async (client, interaction) => {
     const translations = client.language_data(interaction.locale, 'commands/slash/General/fun#game.tic-tac-toe');
+    if (client) throw '啊喔... 我這遊戲還沒製作完成喔(´-ω-`)';
     return await interaction.reply({
         content: '發生了錯誤：因為昨日(06/24)的Discord炸了，所以 柚子Youzi 就沒寫AI的部分了XD',
     });
